548272c9b507a4fb3035605a516ef3f0c984973e
Olga <nekrasova.kalina28@gmail.com>
2022-10-31 12:19:55 +0300

    Create react

---
+++ src/module.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import img from './img/img.png';
+
+const Module = () => {
+    return (
+        <>
+        <h1>Hello react</h1>
+        <img width={300} src={img} />
+        </>
+    )
+}
+
+export default Module;
